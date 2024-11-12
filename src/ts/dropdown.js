8505265e4eb809e8ceb1fa27c088e71dcbd9b1f2
Miriam JC <myriamkey612@gmail.com>
2024-11-12 02:28:23 +0100

    update cv view and design

--- src/ts/dropdown.js
+++ src/ts/dropdown.js
@@ -2,17 +2,29 @@ export const toggleDropdown = () => {
     const dropdowns = document.querySelectorAll('.dropdown');
     dropdowns.forEach(dropdown => {
         const toggleButton = dropdown.querySelector('.dropdown-toggle');
+        const dropdownButtonContent = dropdown.querySelector('.dropdown-btn-content');
         const icon = dropdown.querySelector('.icon');
         const dropdownContent = dropdown.querySelector('.dropdown-content');
         toggleButton.addEventListener('click', () => {
-            dropdownContent.classList.toggle('active');
-            if (dropdownContent.classList.contains('active')) {
+            if (dropdownContent.style.maxHeight === '0px' || dropdownContent.style.maxHeight === '') {
+                dropdownContent.style.maxHeight = `${dropdownContent.scrollHeight}px`;
+            }
+            else {
+                dropdownContent.style.maxHeight = '0';
+            }
+            if (dropdownContent.style.maxHeight !== '0px') {
                 icon.classList.add('fa-caret-up');
                 icon.classList.remove('fa-caret-down');
+                dropdownButtonContent.textContent === 'See more'
+                    ? dropdownButtonContent.textContent = 'See less'
+                    : dropdownButtonContent.textContent = 'See more';
             }
             else {
                 icon.classList.add('fa-caret-down');
                 icon.classList.remove('fa-caret-up');
+                dropdownButtonContent.textContent === 'See less'
+                    ? dropdownButtonContent.textContent = 'See more'
+                    : dropdownButtonContent.textContent = 'See less';
             }
         });
     });
--- src/ts/dropdown.ts
+++ src/ts/dropdown.ts
@@ -1,21 +1,34 @@
 export const toggleDropdown = () => {
-    const dropdowns = document.querySelectorAll('.dropdown');
-  
-    dropdowns.forEach(dropdown => {
-        const toggleButton = dropdown.querySelector('.dropdown-toggle') as HTMLButtonElement;
-        const icon = dropdown.querySelector('.icon') as HTMLElement;
-        const dropdownContent = dropdown.querySelector('.dropdown-content') as HTMLElement;
-  
-        toggleButton.addEventListener('click', () => {
-            dropdownContent.classList.toggle('active');
-            
-            if (dropdownContent.classList.contains('active')) {
-                icon.classList.add('fa-caret-up');
-                icon.classList.remove('fa-caret-down');
-              } else {
-                icon.classList.add('fa-caret-down');
-                icon.classList.remove('fa-caret-up');
-              }
-        });
+  const dropdowns = document.querySelectorAll('.dropdown');
+
+  dropdowns.forEach(dropdown => {
+    const toggleButton = dropdown.querySelector('.dropdown-toggle') as HTMLButtonElement;
+    const dropdownButtonContent = dropdown.querySelector('.dropdown-btn-content') as HTMLElement;
+    const icon = dropdown.querySelector('.icon') as HTMLElement;
+    const dropdownContent = dropdown.querySelector('.dropdown-content') as HTMLElement;
+
+    toggleButton.addEventListener('click', () => {
+      if (dropdownContent.style.maxHeight === '0px' || dropdownContent.style.maxHeight === '') {
+        dropdownContent.style.maxHeight = `${dropdownContent.scrollHeight}px`;
+      } else {
+        dropdownContent.style.maxHeight = '0';
+      }
+
+      if (dropdownContent.style.maxHeight !== '0px') {
+        icon.classList.add('fa-caret-up');
+        icon.classList.remove('fa-caret-down');
+
+        dropdownButtonContent.textContent === 'See more'
+          ? dropdownButtonContent.textContent = 'See less'
+          : dropdownButtonContent.textContent = 'See more';
+      } else {
+        icon.classList.add('fa-caret-down');
+        icon.classList.remove('fa-caret-up');
+
+        dropdownButtonContent.textContent === 'See less'
+          ? dropdownButtonContent.textContent = 'See more'
+          : dropdownButtonContent.textContent = 'See less';
+      }
     });
-  }
+  });
+};
